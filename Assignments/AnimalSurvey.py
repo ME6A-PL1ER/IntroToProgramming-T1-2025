c2cd2753c2fac941fd4ed5db94e0b42fad6e9d94
Owen Rossing <825960@stma.k12.mn.us>
2025-09-29 13:14:10 -0500

    add code comments to survey

--- Assignments/AnimalSurvey.py
+++ Assignments/AnimalSurvey.py
@@ -1,3 +1,4 @@
+# Questions prompted to user
 animal = input("What is your favorite animal?\n: ")
 habitat = input("Where does it live?\n: ")
 diet = input("What does it eat?\n: ")
@@ -9,5 +10,6 @@ covering = input("Does it have fur, feathers, or scales?\n: ")
 classification = input("Is it a mammal, bird, reptile, amphibian, or fish?\n: ")
 sound = input("What sound does it make?\n: ")
 
+# Stitch all questions into one string and print
 summary = f"Your favorite animal is a {animal}. It lives in {habitat}, and it eats {diet}. It is {size} and is {color} in color. It is a {type} animal with {legs} legs. It has {covering} and is classified as a {classification}. The sound it makes is: {sound}."
 print(summary)
