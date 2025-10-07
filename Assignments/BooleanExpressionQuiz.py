1592f98e2f7eebf6f48e4c5455d8a47d5049c879
Owen Rossing <me6apl1er@gmail.com>
2025-10-07 13:50:24 -0500

    3.1 quiz

---
+++ Assignments/BooleanExpressionQuiz.py
@@ -0,0 +1,23 @@
+less5 = int(input("Enter an integer less than 5: "))
+print(less5 < 5)
+
+more5 = int(input("Enter an integer greater than 5: "))
+print(more5 > 5)
+
+equal5 = int(input("Enter an integer equal to 5: "))
+print(equal5 == 5)
+
+not5 = int(input("Enter an integer not equal to 5: "))
+print(not5 != 5)
+
+between3and7 = int(input("Enter an integer between 3 and 7: "))
+print(3 < between3and7 < 7)
+
+sixOrSeven = int(input("Enter an integer equal to 6 or 7: "))
+print(sixOrSeven == 6 or sixOrSeven == 7)
+
+not3or5 = int(input("Enter an integer not equal to 3 or 5: "))
+print(not3or5 != 3 and not3or5 != 5)
+
+sixAndSeven = int(input("Enter an integer equal to 6 and 7: "))
+print(sixAndSeven == 6 and sixAndSeven == 7)
